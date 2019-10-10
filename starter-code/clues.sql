2983531d2a7815e6ec62629d243103b05f54bed5
David Hernandez <david.hernandez8015@gmail.com>
2019-10-10 07:55:36 -0400

    initial commit

---
+++ starter-code/clues.sql
@@ -0,0 +1,123 @@
+-- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
+-- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
+-- so find the least populated country in Southern Europe, and we'll start looking for her there.
+
+select name, min(population) from country where region='Southern Europe' group by name;
+-- answer is Holy See (Vatican City State) |     1000
+
+select name, population from country
+group by name, population
+having country.population = (
+ select min(population) from country
+ where region = 'Southern Europe'
+);
+
+
+-- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
+-- this country's officially recognized language. Check our databases and find out what language is
+-- spoken in this country, so we can call in a translator to work with you.
+
+select language from countrylanguage where countrylanguage.countrycode = 'VAT';
+-- language
+-- ----------
+--  Italian
+-- (1 row)
+
+
+-- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
+-- to a different country, a country where people speak only the language she was learning. Find out which
+--  nearby country speaks nothing but that language.
+select countrycode from countrylanguage where language = 'Italian' and countrylanguage.percentage = 100;
+-- countrycode
+-- -------------
+--  SMR
+-- (1 row)
+
+select * from country where country.code = 'SMR';
+--  SMR  | San Marino | Europe    | Southern Europe |          61 |       885 |      27000 |           81.1 | 510.00 |        | San Marino | Republic       |             |    3171 | SM
+-- (1 row)
+
+
+-- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
+ -- There are only two cities she could be flying to in the country. One is named the same as the country – that
+ -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
+ --  be flying to.
+
+select city.name from city where countrycode = 'SMR';
+--     name
+-- ------------
+--  Serravalle
+--  San Marino
+-- (2 rows)
+
+-- or
+
+select city.name from city where countrycode = 'SMR' and city.name !='San Marino';
+--   name
+-- ------------
+--  Serravalle
+-- (1 row)
+
+
+-- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
+-- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
+-- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
+select city.name from city  where city.name like 'Serra%';
+--     name
+-- ------------
+--  Serra
+--  Serravalle
+-- (2 rows)
+
+select countrycode from city where city.name ='Serra';
+--  countrycode
+-- -------------
+--  BRA
+-- (1 row)
+
+-- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
+ -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
+ -- follow right behind you!
+select country.capital from country where country.code = 'BRA';
+-- capital
+-- ---------
+--      211
+-- (1 row)
+
+select city.name from city where city.id = 211;
+--    name
+-- ------------
+--  Brasï¿½lia (Brasilia)
+-- (1 row)
+
+
+-- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
+ -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
+ -- landing dock.
+
+
+
+-- Clue #8: Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
+-- if we can crack it, we can finally put her where she belongs – behind bars.
+
+
+
+-- Our playdate of late has been unusually fun –
+-- As an agent, I'll say, you've been a joy to outrun.
+-- And while the food here is great, and the people – so nice!
+-- I need a little more sunshine with my slice of life.
+-- So I'm off to add one to the population I find
+-- In a city of ninety-one thousand and now, eighty five.
+
+select city.name from city where city.population = 91084;
+--   name
+-- --------------
+--  Santa Monica
+-- (1 row)
+
+
+-- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
+
+
+-- She's in ____________Santa Monica________________!
